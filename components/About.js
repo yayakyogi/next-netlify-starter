a42578a8ab7f3e136438eb17c447916165ccb9c8
Yayak Yogi <yayaktaka@gmail.com>
2021-08-26 12:48:06 +0700

    Change description

--- components/About.js
+++ components/About.js
@@ -1,17 +1,32 @@
-function About(){
-    return(
-            <div id="about" className="dark:bg-gray-900 bg-white md:bg-gray-100 text-center md:px-32 md:pt-28 pt-16 md:pb-44 pb-36 w-full h-full md:h-screen">
-                <h2 className="dark:text-gray-50 font-black text-4xl text-center mt-8 tracking-wider" data-aos="fade-up">
-                    About me
-                </h2>
-                <div className="h-0.5 dark:bg-gray-500 bg-gray-900 my-10 w-72 mx-auto mb-6" data-aos="fade-up"></div>
-                <div className="mt-12 flex justify-center px-10" data-aos="fade-up">
-                    <p className="dark:text-gray-400 text-gray-700 text-justify leading-loose md:text-lg md:text-center">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam amet sunt ducimus est odio architecto odit iste facilis consequatur ipsa.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, hic dolorum atque velit facere assumenda voluptatibus eaque voluptas ipsam voluptate ea illum veniam? Recusandae, provident doloribus blanditiis unde cum ratione nesciunt magni rem. Consequuntur, impedit. Commodi molestias odit neque mollitia magni, quod molestiae eligendi ex, earum aliquid optio quidem. Officiis?
-                    </p>
-                </div>
-            </div>
-    )
+function About() {
+  return (
+    <div
+      id="about"
+      className="dark:bg-gray-900 bg-white md:bg-gray-100 text-center md:px-32 md:pt-28 pt-16 md:pb-44 pb-36 w-full h-full md:h-screen"
+    >
+      <h2
+        className="dark:text-gray-50 font-black text-4xl text-center mt-8 tracking-wider"
+        data-aos="fade-up"
+      >
+        About me
+      </h2>
+      <div
+        className="h-0.5 dark:bg-gray-500 bg-gray-900 my-10 w-72 mx-auto mb-6"
+        data-aos="fade-up"
+      ></div>
+      <div className="mt-12 flex justify-center px-10" data-aos="fade-up">
+        <p className="dark:text-gray-400 text-gray-700 text-justify leading-loose md:text-lg md:text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam amet
+          sunt ducimus est odio architecto odit iste facilis consequatur ipsa.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
+          hic dolorum atque velit facere assumenda voluptatibus eaque voluptas
+          ipsam voluptate ea illum veniam? Recusandae, provident doloribus
+          blanditiis unde cum ratione nesciunt magni rem. Consequuntur, impedit.
+          Commodi molestias odit neque mollitia magni, quod molestiae eligendi
+          ex, earum aliquid optio quidem. Officiis?
+        </p>
+      </div>
+    </div>
+  );
 }
-export default About
+export default About;
